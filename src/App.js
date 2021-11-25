b9be25e85bbec76ab350133c95e660d77826745b
Vasiliy Buriy <vasilyrd2001@gmail.com>
2021-11-25 10:06:12 +0200

    create project using ReactBootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div >
+      
+        <Button>Text</Button>
+        
+    </div>
+  );
+}
+
+export default App;
